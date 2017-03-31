330c5b21622ae0a8cf060c2536d63c3da698d304
bzachmann <boston.a.zachmann@ndsu.edu>
2017-03-30 22:45:00 -0500

    added comments on some TODO's and issues

--- ProjectCode/ProjectCode.ino
+++ ProjectCode/ProjectCode.ino
@@ -1,3 +1,11 @@
+/*
+ * TODO:
+ * 
+ * 1. check hall effect sensor, and if not present trigger shutdown
+ * 2. figure out way to cancel rocket spin using angular velocity about the x axis
+ * 3. figure out shaking issue that is caused if removing the delay() in the main loop
+ */
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -27,26 +35,29 @@ BetterServo servo[4];
 sensors_event_t event;
 double zeroPoint[2] = {0.0, 0.0};
 double deviation[2] = {0.0, 0.0};
-uint32_t buttonCount = 0;
-bool zeroPointSetFlag = false;
+volatile uint32_t buttonCount = 0;
+volatile bool setZeroPointFlag = false;
 
 ///////////////prototypes//////////////////
 void setupServos();
 void setServosTilt(double y, double z);
 void setZeroPoint();
+void setupTimer2();
+void checkButton();
+void checkSetZeroPoint();
 
 
 //////////////program///////////////////
 void setup() {
   
+  setupTimer2();
   setupServos();
 
   if(!bno.begin())
   {
     while(1);
   }
-
-  delay(1000);
+  delay(200);
   bno.setExtCrystalUse(true);
 }
 
@@ -55,25 +66,14 @@ void loop()
   bno.getEvent(&event);
   deviation[0] = event.orientation.y - zeroPoint[0];
   deviation[1] = event.orientation.z - zeroPoint[1];
+  
   setServosTilt(deviation[0], deviation[1]);
-  
-
-  if(digitalRead(PIN_BUTTON))
-  {
-    buttonCount++;
-    if(buttonCount == 150) //3 sec b/c of 10ms delay at end of loop.. change this when timer interrupt implemented
-    {
-      setZeroPoint();
-      buttonCount = 0;
-    }
-  }
-  else
-  {
-    buttonCount = 0;
-  }
-  
-  delay(10);
-
+  checkSetZeroPoint();
+  delay(5);//without this delay the servos jitter.  I'm guessing that without this delay, the servo values being changed
+            // too often and it is messing up the PWM in the middle of the cycle.  I also tried a interrup driven "wait to update"
+            // but that also caused shaking and also the update time was terrible.  Like 500 ms instead of 5ms.  Not sure if the
+            //timer interrupt is being handled as often as we think due to the other interrupts on the system.  This would be
+            //something to look into.
 }
 
 void setupServos()
@@ -108,5 +108,55 @@ void setZeroPoint()
   zeroPoint[0] = event.orientation.y;
   zeroPoint[1] = event.orientation.z;
 }
+
+void setupTimer2()
+{
+  TCCR2B = 0x00;        //Disbale Timer2 while we set it up
+  TCNT2  = 130;         //Reset Timer Count to 130 out of 255(1 ms)
+  TIFR2  = 0x00;        //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+  TIMSK2 = 0x01;        //Timer2 INT Reg: Timer2 Overflow Interrupt Enable
+  TCCR2A = 0x00;        //Timer2 Control Reg A: Wave Gen Mode normal
+  TCCR2B = 0x05;        //Timer2 Control Reg B: Timer Prescaler set to 128
+}
+
+void checkButton()
+{
+  if(digitalRead(PIN_BUTTON))
+  {
+    buttonCount++;
+    if(buttonCount == 1500)
+    {
+      setZeroPointFlag = true;
+      buttonCount = 0;
+    }
+  }
+  else
+  {
+    buttonCount = 0;
+  }
+}
+
+void checkSetZeroPoint()
+{
+  if(setZeroPointFlag)
+  {
+    setZeroPoint();
+    setZeroPointFlag = false;
+  }
+}
+
+//Timer2 Overflow Interrupt Vector, called every 1ms
+ISR(TIMER2_OVF_vect)        // interrupt service routine 
+{ 
+  TCNT2 = 130;           //Reset Timer to 130 out of 255
+  
+  //we want to do a couple things with the interrupt
+  //2. every 10 interrupts, log the deviation values to a queue that will be then sent to the sd card whenever possible
+  //3. check if the magnet is no longer present after a debounce, similar to checkButton().  If not, trigger shutdown flag.
+  
+  checkButton();
+    
+  TIFR2 = 0x00;          //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+};
 
 
