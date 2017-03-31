1d25d5192f9525e43ce34bb5ef303dfc15affca7
bzachmann <boston.a.zachmann@ndsu.edu>
2017-03-30 19:52:59 -0500

    imu adjusts servo

--- ModifiedServoLib/ModifiedServoLib.ino
+++ ModifiedServoLib/ModifiedServoLib.ino
@@ -1,33 +1,39 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 #include "BetterServo.h"
 
-BetterServo servo = BetterServo();
-double val = 0.0;
 //test servo addition offset 
 #define OFFSET_ALL -90
 #define OFFSET_SERVO 15
 
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+BetterServo servo = BetterServo();
+double val = 0.0;
+
 void setup() {
   pinMode(13, OUTPUT);
-  servo.attach(11, -30.0, 30.0, OFFSET_ALL + OFFSET_SERVO); //7.5 offset
+  servo.attach(11, -50.0, 50.0, OFFSET_ALL + OFFSET_SERVO); //7.5 offset
   Serial.begin(9600);
+
+  if(!bno.begin())
+  {
+    Serial.print("bno not connected");
+    while(1);
+  }
+
+  delay(1000);
+  bno.setExtCrystalUse(true);
 }
 
 void loop() {
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  bno.getEvent(&event);
+  servo.write(event.orientation.z);
   
-  servo.write(val);
-  while(Serial.available())
-  {
-    char myChar = Serial.read();
-    if(myChar == '-')
-    {
-      val = val - 0.5;
-      Serial.println(val);
-    }
-    else if(myChar == '+')
-    {
-      val = val + 0.5;
-      Serial.println(val);
-    }
-  }
+  delay(10);
 }
